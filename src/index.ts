fa9103a4fc46bdc3fca67e6e526088860578dbca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-05-04 13:36:35 -0700

    fix: speed up execution by checking whether decorators or ctorParameters exist first

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import {scrubFile} from './ngo';
 const tmp = require('tmp');
 const fs = require('fs');
 
+const HAS_DECORATORS = /decorators/;
+const HAS_CTOR_PARAMETERS = /ctorParameters/;
+
 module.exports = function(content: string) {
-  const tmpFile = tmp.fileSync({postfix: '.js'}).name;
-  console.log('temp file', this.request, tmpFile);
-  fs.writeFileSync(tmpFile, content);
-  return scrubFile(tmpFile, this.request);
+  if (HAS_DECORATORS.test(content) || HAS_CTOR_PARAMETERS.test(content)) {
+    const tmpFile = tmp.fileSync({postfix: '.js'}).name;
+    console.log('temp file', this.request, tmpFile);
+    fs.writeFileSync(tmpFile, content);
+    return scrubFile(tmpFile, this.request);
+  }
+  return content;
 }
