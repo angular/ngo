6572e21fabad202c680fee9797b05a05b97c389a
Filipe Silva <filipematossilva@gmail.com>
2017-05-25 13:27:11 +0100

    add remaining decorator tests

--- src/ngo.spec.ts
+++ src/ngo.spec.ts
@@ -13,18 +13,54 @@ function repeatSpace(count: number) {
 }
 
 describe('ngo', () => {
-  it('replaces decorators with spaces', () => {
-    const decorator = 'Clazz.decorators = [ { type: Injectable } ];';
-    const input = `
-      import { Injectable } from '@angular/core';var Clazz = (function () { function Clazz() { } return Clazz; }());
-      ${decorator}
-    `;
-    const output = input.replace(decorator, repeatSpace(decorator.length));
+  describe('decorators', () => {
+    const clazz = 'var Clazz = (function () { function Clazz() { } return Clazz; }());';
 
-    const tmpFile = tmp.fileSync({ postfix: '.js' }).name;
-    fs.writeFileSync(tmpFile, input);
+    it('replaces Angular decorators with spaces', () => {
+      const decorators = 'Clazz.decorators = [ { type: Injectable } ];';
+      const input = `
+        import { Injectable } from '@angular/core';
+        ${clazz}
+        ${decorators}
+      `;
+      const output = input.replace(decorators, repeatSpace(decorators.length));
 
-    expect(scrubFile(tmpFile, 'spec')).toEqual(output);
+      const tmpFile = tmp.fileSync({ postfix: '.js' }).name;
+      fs.writeFileSync(tmpFile, input);
+
+      expect(scrubFile(tmpFile, 'spec')).toEqual(output);
+    });
+
+    it('doesn\'t replace non Angular decorators', () => {
+      const decorators = 'Clazz.decorators = [ { type: Injectable } ];';
+      const input = `
+        import { Injectable } from 'another-lib';
+        ${clazz}
+        ${decorators}
+      `;
+
+      const tmpFile = tmp.fileSync({ postfix: '.js' }).name;
+      fs.writeFileSync(tmpFile, input);
+
+      expect(scrubFile(tmpFile, 'spec')).toEqual(input);
+    });
+
+    it('leaves non-Angulars decorators in mixed arrays', () => {
+      const angularDecorator = '{ type: Injectable }';
+      const decorators = `Clazz.decorators = [ ${angularDecorator}, { type: NotInjectable } ];`;
+      const input = `
+        import { Injectable } from '@angular/core';
+        import { NotInjectable } from 'another-lib';
+        ${clazz}
+        ${decorators}
+      `;
+      const output = input.replace(angularDecorator, repeatSpace(angularDecorator.length));
+
+      const tmpFile = tmp.fileSync({ postfix: '.js' }).name;
+      fs.writeFileSync(tmpFile, input);
+
+      expect(scrubFile(tmpFile, 'spec')).toEqual(output);
+    });
   });
 });
 
