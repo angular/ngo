8a18167a4314609601c97db8071b12b9257ad532
Filipe Silva <filipematossilva@gmail.com>
2017-05-25 11:06:31 +0100

    add test setup

---
+++ scripts/run-spec.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const Jasmine = require('jasmine');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const tsNode = require('ts-node');
+
+const projectRoot = path.join(__dirname, '../');
+const projectBaseDir = path.join(projectRoot, 'src');
+
+// Register ts-node so typescript is compiled on the fly.
+require('ts-node').register({ project: path.join(projectRoot, 'tsconfig.spec.json') })
+
+// Create a Jasmine runner and configure it.
+const jasmine = new Jasmine({ projectBaseDir });
+jasmine.loadConfig({});
+jasmine.addReporter(new SpecReporter({ spec: { displayPending: true } }));
+
+// Run the tests.
+jasmine.execute([path.join(projectBaseDir, '**/*.spec.ts')]);
---
+++ src/class-fold.spec.ts
@@ -0,0 +1,5 @@
+describe('class-fold', () => {
+  it('works', () => {
+    expect(true).toEqual(true);
+  });
+});
