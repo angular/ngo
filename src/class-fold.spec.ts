6bb52ecf847b4bf9efeb27756b661777ee541cd9
Filipe Silva <filipematossilva@gmail.com>
2017-05-25 12:46:40 +0100

    add class-fold spec

--- src/class-fold.spec.ts
+++ src/class-fold.spec.ts
@@ -1,5 +1,20 @@
+import { stripIndent } from 'common-tags';
+import * as tmp from 'tmp';
+import * as fs from 'fs';
+
+import { foldFile } from './class-fold';
+
+
 describe('class-fold', () => {
   it('works', () => {
-    expect(true).toEqual(true);
+    const input =
+      'var Clazz = (function () { function Clazz() { } return Clazz; }());\nClazz.prop = 1;';
+    const output =
+      'var Clazz = (function () { function Clazz() { } Clazz.prop = 1;return Clazz; }());\n';
+
+    const tmpFile = tmp.fileSync({ postfix: '.js' }).name;
+    fs.writeFileSync(tmpFile, input);
+
+    expect(foldFile(tmpFile, 'spec')).toEqual(output);
   });
 });
