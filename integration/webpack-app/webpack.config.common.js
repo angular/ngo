e3e395f15dbf89b9ce008af6bf0a195476f09c8a
Filipe Silva <filipematossilva@gmail.com>
2017-05-29 13:23:54 +0100

    add vendor bundle

--- integration/webpack-app/webpack.config.common.js
+++ integration/webpack-app/webpack.config.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ngToolsWebpack = require('@ngtools/webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
@@ -6,6 +7,8 @@ const CompressionPlugin = require('compression-webpack-plugin');
 
 const src = path.resolve(__dirname, 'src/');
 const dist = path.resolve(__dirname, 'dist/');
+const nodeModules = path.resolve(__dirname, 'node_modules/');
+const genDirNodeModules = path.resolve(__dirname, 'src', '$$_gendir');
 
 module.exports = {
   resolve: {
@@ -22,6 +25,12 @@ module.exports = {
     }),
     new ngToolsWebpack.AotPlugin({
       tsConfigPath: path.join(src, 'tsconfig.json')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      chunks: ['main'],
+      minChunks: (module) => module.resource &&
+        (module.resource.startsWith(nodeModules) || module.resource.startsWith(genDirNodeModules))
     }),
     new CompressionPlugin({
         asset: '[path].gz[query]',
