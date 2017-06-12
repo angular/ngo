37b7507be4088966e3e54cc12c62e18ed16f8374
Filipe Silva <filipematossilva@gmail.com>
2017-06-12 19:21:25 +0100

    feat: emit .d.ts files (#6)

---
+++ dist/class-fold.d.ts
@@ -0,0 +1,2 @@
+import * as ts from 'typescript';
+export declare function getFoldFileTransformer(program: ts.Program): ts.TransformerFactory<ts.SourceFile>;
---
+++ dist/ngo-webpack-loader.d.ts
@@ -0,0 +1,2 @@
+import { RawSourceMap } from 'source-map';
+export declare function ngoLoader(content: string, previousSourceMap: RawSourceMap): void;
