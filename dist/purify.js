8a19e7db2758d931de198016ab07d81734d6ebbf
Filipe Silva <filipematossilva@gmail.com>
2017-06-13 16:57:57 +0100

    feat: add new purify optimizations
    Contains changes from these commits:
    
    https://github.com/IgorMinar/purify/compare/2d5c1daa858031693dfb21c62cbac2fdda4cf09f...1b4f00d9c29efad03acc10a69ce280bdf59fc9cb

--- dist/purify.js
+++ dist/purify.js
@@ -6,14 +6,18 @@ var importCommentRegex = /\/\*\* PURE_IMPORTS_START (\S+) PURE_IMPORTS_END \*\//
 function purify(content) {
     var pureImportMatches = getMatches(content, importCommentRegex, 1).join('|');
     var newContent = content
+        .replace(
+    // tslint:disable-next-line:max-line-length
+    /^(var (\S+) = )(\(function \(\) \{\r?\n(?:    (?:\/\*\*| \*|\*\/|\/\/)[^\r?\n]*\r?\n)*    function \2\([^\)]*\) \{\r?\n)/mg, '$1/*@__PURE__*/$3')
+        .replace(/^(var (\S+) = )(\(function \(_super\) \{\r?\n    \w*__extends\(\w+, _super\);\r?\n)/mg, '$1/*@__PURE__*/$3')
         .replace(/var (\S+) = \{\};\r?\n(\1\.(\S+) = \d+;\r?\n)+\1\[\1\.(\S+)\] = "\4";\r?\n(\1\[\1\.(\S+)\] = "\S+";\r?\n*)+/mg, 'var $1 = /*@__PURE__*/(function() {\n$&; return $1;})();\n')
         .replace(
     // tslint:disable-next-line:max-line-length
     /var (\S+);(\/\*@__PURE__\*\/)*\r?\n\(function \(\1\) \{\s+(\1\[\1\["(\S+)"\] = 0\] = "\4";(\s+\1\[\1\["\S+"\] = \d\] = "\S+";)*\r?\n)\}\)\(\1 \|\| \(\1 = \{\}\)\);/mg, 'var $1 = /*@__PURE__*/(function() {\n    var $1 = {};\n    $3    return $1;\n})();')
         .replace(new RegExp("(_(" + pureImportMatches + ")__ = )(__webpack_require__\\(\\S+\\);)", 'mg'), '$1/*@__PURE__*/$3')
         .replace(new RegExp("(_(" + pureImportMatches + ")___default = )(__webpack_require__\\.\\w\\(\\S+\\);)", 'mg'), '$1/*@__PURE__*/$3')
-        .replace(/__WEBPACK_IMPORTED_MODULE_\d+__angular_core__\["_?\w+" \/\* (ɵccf|ɵcmf) \*\/\]\(/mg, '/*@__PURE__*/$&')
-        .replace(/\/\*\*\r?\n\s*\*\s?@license.*\r?\n(\s*\*[^\/].*\r?\n)*\s*\*\//mg, '\n');
+        .replace(/\w*__WEBPACK_IMPORTED_MODULE_\d+__angular_core__\["\w+" \/\* (ɵccf|ɵcmf) \*\/\]\(/mg, '/*@__PURE__*/$&')
+        .replace(/new \w*__WEBPACK_IMPORTED_MODULE_\d+__angular_core__\["\w+" \/\* NgModuleFactory \*\/\]/mg, '/*@__PURE__*/$&');
     return newContent;
 }
 exports.purify = purify;
